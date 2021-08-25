54396b236eedf115afebc66b75f58773935b8ed3
Soobin <soobin9711@gmail.com>
2021-08-25 15:21:51 +0900

    업데이트 알림창, 삭제 경고창

--- mint-web/src/test/java/com/study/mint/mintweb/MintWebApplicationTests.java
+++ mint-web/src/test/java/com/study/mint/mintweb/MintWebApplicationTests.java
@@ -1,10 +1,8 @@
 package com.study.mint.mintweb;
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MintWebApplicationTests {
